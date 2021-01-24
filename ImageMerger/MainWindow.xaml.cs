7de63dda47b37dbff1946ee452dbb2e0e975f1e3
ShaDream <russianboysasha@yandex.ru>
2021-01-24 16:06:23 +0500

    Add logic for Slice

--- ImageMerger/MainWindow.xaml.cs
+++ ImageMerger/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Colourful;
+using Colourful.Conversion;
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
@@ -76,7 +79,11 @@ namespace ImageMerger
 
             bw.RunWorkerCompleted += (s, args) =>
             {
-                DoneBT.Dispatcher.Invoke(() => DoneBT.IsEnabled = true);
+                DoneBT.Dispatcher.Invoke(() =>
+                {
+                    DoneBT.IsEnabled = true;
+                    SystemSounds.Asterisk.Play();
+                });
             };
 
             if (ConcatMode.IsChecked.Value)
@@ -94,7 +101,6 @@ namespace ImageMerger
                 };
             }
             bw.RunWorkerAsync();
-
         }
 
         private void ConcatImages()
@@ -104,6 +110,7 @@ namespace ImageMerger
             var offsetValue = offset.Dispatcher.Invoke(() => offset.Value.Value);
             var files = Directory.GetFiles(inDir).Where(x => Extensions.Contains(System.IO.Path.GetExtension(x))).ToArray();
             var maxFiles = files.Length;
+            var concatedFiles = 0;
 
             int i = 1;
             int pad = 2;
@@ -112,8 +119,11 @@ namespace ImageMerger
             {
                 var mergingFiles = TakeImagesForConcat(files);
                 files = files.Skip(mergingFiles.Length).ToArray();
-                Progress.Dispatcher.Invoke(() => Progress.Value += (double)mergingFiles.Length / maxFiles);
+                concatedFiles += mergingFiles.Length;
                 var result = Merge(mergingFiles, offsetValue);
+
+                Progress.Dispatcher.Invoke(() => Progress.Value = (double)concatedFiles / maxFiles);
+
                 var path = $"{outDIrectory}\\{i.ToString().PadLeft(pad, '0')}.png";
                 result.Save(path);
                 result.Dispose();
@@ -123,7 +133,6 @@ namespace ImageMerger
                 }
                 i++;
             }
-            SystemSounds.Asterisk.Play();
 
         }
 
@@ -144,7 +153,7 @@ namespace ImageMerger
                 while (maxHeightConcatValue > currentHeight)
                 {
                     var newBitmap = new Bitmap(files[curIndex]);
-                    currentHeight += newBitmap.Width;
+                    currentHeight += newBitmap.Height;
                     bitmaps.Add(newBitmap);
                 }
                 bitmaps[bitmaps.Count - 1].Dispose();
@@ -155,7 +164,184 @@ namespace ImageMerger
 
         private void SliceImages()
         {
+            var inDir = ImageDir.Dispatcher.Invoke(() => ImageDir.Text);
+            var outDIrectory = ImageDir.Dispatcher.Invoke(() => outDir.Text);
+            var isTryTrueSlice = TryTrueSlice.Dispatcher.Invoke(() => TryTrueSlice.IsChecked.Value);
+            var sliceCount = SliceCount.Dispatcher.Invoke(() => SliceCount.Value.Value);
+            var trueSliceMaxDistance = TrueSliceMaxDistance.Dispatcher.Invoke(() => TrueSliceMaxDistance.Value.Value);
+            var sliceMinHeight = SliceMinHeight.Dispatcher.Invoke(() => SliceMinHeight.Value.Value);
+            var sliceMaxHeight = SliceMaxHeight.Dispatcher.Invoke(() => SliceMaxHeight.Value.Value);
+            sliceMaxHeight = sliceMaxHeight == 0 ? 0 : Math.Max(sliceMinHeight, sliceMaxHeight);
+            var colorDiff = TrueSliceColorDifference.Dispatcher.Invoke(() => TrueSliceColorDifference.Value.Value);
+            var trueSliceHeight = TrueSliceHeight.Dispatcher.Invoke(() => TrueSliceHeight.Value.Value);
 
+            var slicedFiles = 0;
+            var files = Directory.GetFiles(inDir).Where(x => Extensions.Contains(System.IO.Path.GetExtension(x))).ToArray();
+            var maxFiles = files.Length;
+
+            int i = 1;
+            int pad = 2;
+
+            if (!isTryTrueSlice)
+                foreach (var imagePath in files)
+                {
+                    var image = new Bitmap(imagePath);
+                    var slices = new List<int>();
+                    for (int c = 1; c < sliceCount; c++)
+                    {
+                        var slice = (int)((double)c / sliceCount * image.Height);
+                        slices.Add(slice);
+                    }
+                    slices.Add(image.Height);
+                    var resultImages = Slice(image, slices.ToArray());
+
+                    slicedFiles++;
+                    Progress.Dispatcher.Invoke(() => Progress.Value = (double)slicedFiles / maxFiles);
+
+                    foreach (var resImage in resultImages)
+                    {
+                        var path = $"{outDIrectory}\\{i.ToString().PadLeft(pad, '0')}.png";
+                        resImage.Save(path);
+                        i++;
+                        resImage.Dispose();
+                    }
+                }
+            else
+            {
+                foreach (var imagePath in files)
+                {
+                    var image = new Bitmap(imagePath);
+                    var slices = new List<int>();
+                    var previousSlice = 0;
+                    while (previousSlice != image.Height)
+                    {
+                        var minMaxSlice = GetMinMaxHeight(previousSlice, image.Height, sliceCount, trueSliceMaxDistance, sliceMinHeight, sliceMaxHeight, previousSlice);
+                        var slice = GetSlice(image, minMaxSlice.Min, minMaxSlice.Max, trueSliceHeight, colorDiff);
+                        if (slice == -1)
+                            slice = minMaxSlice.Min + (minMaxSlice.Max - minMaxSlice.Min) / 2;
+                        previousSlice = slice;
+                        slices.Add(previousSlice);
+                    }
+
+                    var resultImages = Slice(image, slices.ToArray());
+
+                    slicedFiles++;
+                    Progress.Dispatcher.Invoke(() => Progress.Value = (double)slicedFiles / maxFiles);
+
+                    foreach (var resImage in resultImages)
+                    {
+                        var path = $"{outDIrectory}\\{i.ToString().PadLeft(pad, '0')}.png";
+                        resImage.Save(path);
+                        i++;
+                        resImage.Dispose();
+                    }
+                }
+            }
+        }
+
+        private (int Min, int Max) GetMinMaxHeight(int previousSlice, int imageHeight, int sliceCount, int trueSliceMaxDistance, int sliceMinHeight, int sliceMaxHeight, int absoluteMin)
+        {
+            if (sliceMaxHeight == 0)
+            {
+                var sliceStep = imageHeight / sliceCount;
+                var nextSliceCenter = previousSlice + sliceStep;
+
+                var min = Clamp(nextSliceCenter - trueSliceMaxDistance / 2, absoluteMin, imageHeight);
+                var max = Clamp(nextSliceCenter + trueSliceMaxDistance / 2, absoluteMin, imageHeight);
+
+                if (imageHeight - max < sliceMinHeight)
+                    return (imageHeight, imageHeight);
+                
+                return (min, max);
+            }
+            else
+            {
+                var min = Clamp(previousSlice + sliceMinHeight, absoluteMin, imageHeight);
+                var max = Clamp(previousSlice + sliceMaxHeight, absoluteMin, imageHeight);
+
+                if (imageHeight - max < sliceMinHeight)
+                    return (imageHeight, imageHeight);
+
+                return (min, max);
+            }
+        }
+
+        private int GetSlice(Bitmap image, int MinHeight, int MaxHeight, int lineWidth, int maxColorDifference)
+        {
+            for (var height = MaxHeight; height >= MinHeight; height--)
+                if (IsGoodLine(image, lineWidth, height, maxColorDifference))
+                    return height;
+
+            return -1;
+        }
+
+        private static int Clamp(int value, int min, int max)
+        {
+            return Math.Max(Math.Min(value, max), min);
+        }
+
+        private bool IsGoodLine(Bitmap image, int lineWidth, int lineHeight, int colorDifferenceTreshold)
+        {
+            var imageHeight = image.Height;
+            System.Drawing.Color zeroColor = System.Drawing.Color.FromArgb(0, 0, 0, 0);
+            int lineMin = lineHeight - lineWidth / 2;
+            int lineMax = lineHeight + lineWidth / 2;
+            var mainColor = zeroColor;
+
+            for (var y = Math.Max(lineMin, 0); y <= lineMax && y < imageHeight; y++)
+            {
+                for (var x = 0; x < image.Width; x++)
+                {
+                    if (mainColor == zeroColor)
+                    {
+                        mainColor = image.GetPixel(x, y);
+                    }
+                    else
+                    {
+                        var colorDiff = (int)ColorDifference(mainColor, image.GetPixel(x, y));
+                        if (colorDiff > colorDifferenceTreshold)
+                            return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        private static double ColorDifference(System.Drawing.Color a, System.Drawing.Color b)
+        {
+            var aNormalized = new double[] { (double)a.R / 255, (double)a.G / 255, (double)a.B / 255 };
+            var bNormalized = new double[] { (double)b.R / 255, (double)b.G / 255, (double)b.B / 255 };
+            RGBColor aColor = new RGBColor(aNormalized[0], aNormalized[1], aNormalized[2]);
+            RGBColor bColor = new RGBColor(bNormalized[0], bNormalized[1], bNormalized[2]);
+
+            var converter = new ColourfulConverter { WhitePoint = Illuminants.D65 };
+            var labA = converter.ToLab(aColor);
+            var labB = converter.ToLab(bColor);
+
+            return Math.Sqrt(Math.Pow(labB.L - labA.L, 2) + Math.Pow(labB.a - labA.a, 2) + Math.Pow(labB.b - labA.b, 2));
+        }
+
+        private static Bitmap[] Slice(Bitmap image, int[] slices)
+        {
+            if (slices == null || slices.Length == 0)
+                return new Bitmap[] { image };
+
+            Array.Sort(slices);
+            var bitmaps = new List<Bitmap>();
+            var prevSlice = 0;
+            for (int i = 0; i < slices.Length; i++)
+            {
+                var slice = slices[i];
+                Bitmap sliceImage = new Bitmap(image.Width, slice - prevSlice);
+                using (var g = Graphics.FromImage(sliceImage))
+                {
+                    g.DrawImage(image, new RectangleF(0, 0, image.Width, slice - prevSlice), new RectangleF(0, prevSlice, image.Width, slice - prevSlice), GraphicsUnit.Pixel);
+                }
+                prevSlice = slice;
+                bitmaps.Add(sliceImage);
+            }
+
+            return bitmaps.ToArray();
         }
 
         private static Bitmap Merge(Bitmap[] bitmaps, int offset)
